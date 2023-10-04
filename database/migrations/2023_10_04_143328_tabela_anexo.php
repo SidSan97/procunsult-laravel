c4c0202f285730ac4ee9b8e623c243c99d6e66a4
SidSan97 <sidnei1.8santiago@hotmail.com>
2023-10-04 12:03:07 -0300

    Criaçao das tabelas de chamados e anexos

---
+++ database/migrations/2023_10_04_143328_tabela_anexo.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('anexos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('chamado_id')->nullable();
+            $table->string('nome_anexo')->nullable();
+
+            // Definir a chave estrangeira
+            $table->foreign('chamado_id')
+                  ->references('id')
+                  ->on('chamados')
+                  ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
