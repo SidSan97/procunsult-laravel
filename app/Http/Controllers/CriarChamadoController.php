bed4d83c69abac999fcd252874d96f2b97ce2e08
SidSan97 <sidnei1.8santiago@hotmail.com>
2023-10-04 19:12:50 -0300

    Adiçao de classe de criaçao de chamada

--- app/Http/Controllers/CriarChamadoController.php
+++ app/Http/Controllers/CriarChamadoController.php
@@ -10,5 +10,15 @@ class CriarChamadoController extends Controller
     public function abrirChamado(Request $request) {
 
         $user = new CriarChamadoModel();
+
+        $user->titulo    = $request->titulo;
+        $user->descricao = $request->descricao;
+        $user->resposta  = $request->resposta;
+        $user->status    = $request->status;
+
+        if($user->save()) {
+
+
+        }
     }
 }
