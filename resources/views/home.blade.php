9f55c8522c9b7160c8ce5c5124583bef52988717
SidSan97 <sidnei1.8santiago@hotmail.com>
2023-10-05 18:04:36 -0300

    Criando rota de logout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,9 +7,10 @@
     <title>Home</title>
 </head>
 <body>
-    <h1>Você precisa estar logado para usar o sistema</h1> <br>
 
     @guest
+    <h1>Você precisa estar logado para usar o sistema</h1> <br>
+
     <div>
         <p><a href="/login">Login</a></p>
         <p><a href="/register">Registrar</a></p>
@@ -19,10 +20,29 @@
     <br>
 
     @auth
+    <h1>Bem-vindo, {{$user = Auth::user()->name;}}</h1> <br>
+    <h3>Nivel: {{$user = Auth::user()->nivel;}}</h3>
+
     <div>
-        <p><a href="/abrir-chamado">Abrir chamado</a></p>
-        <p><a href="/responder-chamado">Responder chamado</a></p>
+        @if($user = Auth::user()->nivel == "Cliente")
+            <p><a href="/abrir-chamado">Abrir chamado</a></p>
+        @elseif($user = Auth::user()->nivel == "Colaborador")
+            <p><a href="/responder-chamado">Responder chamado</a></p>
+        @endif
     </div>
+
+    <form method="POST" action="{{ route('logout') }}">
+        @csrf
+        <div class="nav-item">
+            <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                        this.closest('form').submit(); " role="button">
+                <i class="fas fa-sign-out-alt"></i>
+
+                {{ __('Log Out') }}
+            </a>
+        </div>
+    </form>
+
     @endauth
 </body>
 </html>
