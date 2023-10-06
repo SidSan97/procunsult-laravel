152bf0f4d3c2284a02be175734b2c50237e58c5a
SidSan97 <sidnei1.8santiago@hotmail.com>
2023-10-06 14:39:25 -0300

    Atualziando estrutura

--- dump/procunsult_laravel.sql
+++ dump/procunsult_laravel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 06-Out-2023 às 18:19
+-- Tempo de geração: 06-Out-2023 às 19:38
 -- Versão do servidor: 10.4.24-MariaDB
 -- versão do PHP: 8.1.6
 
