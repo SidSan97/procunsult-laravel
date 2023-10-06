238a03b9d100c1747b9c15d67de3cd2f2ca3441e
SidSan97 <sidnei1.8santiago@hotmail.com>
2023-10-06 13:01:32 -0300

    Validador de cpf

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,12 +21,34 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Validator::extend('cpf', function ($attribute, $value, $parameters, $validator) {
-            // Lógica de validação do CPF
-            // Você pode usar uma biblioteca externa ou escrever sua própria validação aqui
-            // Retorne true se o CPF for válido e false caso contrário
+         Validator::extend('cpf', function ($attribute, $value, $parameters, $validator) {
+            $cpf = $value;
+            // Extrai somente os números
+            $cpf = preg_replace( '/[^0-9]/is', '', $cpf );
+
+            // Verifica se foi informado todos os digitos corretamente
+            if (strlen($cpf) != 11) {
+                return false;
+            }
+
+            // Verifica se foi informada uma sequência de digitos repetidos. Ex: 111.111.111-11
+            if (preg_match('/(\d)\1{10}/', $cpf)) {
+                return false;
+            }
+
+            // Faz o calculo para validar o CPF
+            for ($t = 9; $t < 11; $t++) {
+                for ($d = 0, $c = 0; $c < $t; $c++) {
+                    $d += $cpf[$c] * (($t + 1) - $c);
+                }
+                $d = ((10 * $d) % 11) % 10;
+                if ($cpf[$c] != $d) {
+                    return false;
+                }
+            }
             return true;
         });
+
 
         Validator::extend('nivel', function ($value) {
 
