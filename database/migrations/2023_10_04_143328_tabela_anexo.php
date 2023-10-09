5b7584a86dc68f942a67936d186bbf9924f09b73
SidSan97 <sidnei1.8santiago@hotmail.com>
2023-10-08 22:08:10 -0300

    update

--- database/migrations/2023_10_04_143328_tabela_anexo.php
+++ database/migrations/2023_10_04_143328_tabela_anexo.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('chamado_id')->nullable();
             $table->string('nome_anexo')->nullable();
+            $table->timestamps();
 
             // Definir a chave estrangeira
             $table->foreign('chamado_id')
